9b537a7c0e5780b54bae4110fedd4f9e802f0f54
Christopher Philp <chris.philp@starlingbank.com>
2021-11-14 17:52:42 +0000

    [main] | Adding app.yaml for gcloud deploy

--- src/main.py
+++ src/main.py
@@ -1,14 +1,27 @@
 from rightmove import RightmovePropertiesForSale
 from zoopla import ZooplaPropertiesForSale
+from flask import Flask
 
-# Rightmove Properties
-RightmovePropertiesForSale(location_identifier='REGION^93929', radius_from_location=0, )  # barnet
-RightmovePropertiesForSale(location_identifier='REGION^1017', radius_from_location=1, )  # northwood
-RightmovePropertiesForSale(location_identifier='REGION^1154', radius_from_location=1, )  # ruislip
-RightmovePropertiesForSale(location_identifier='REGION^79781', radius_from_location=0.5, )  # harrow_on_the_hill
+app = Flask(__name__)
 
-# Zoopla Properties
-ZooplaPropertiesForSale(location_identifier='barnet-london-borough', radius_from_location=0, include_sstc=False)  # barnet
-ZooplaPropertiesForSale(location_identifier='london/northwood', radius_from_location=1, include_sstc=False)  # northwood
-ZooplaPropertiesForSale(location_identifier='ruislip', radius_from_location=1, include_sstc=False)  # ruislip
-ZooplaPropertiesForSale(location_identifier='harrow-on-the-hill', radius_from_location=1, include_sstc=False)  # harrow-on-the-hill
+
+@app.route('/')
+def main():
+    # Rightmove Properties
+    RightmovePropertiesForSale(location_identifier='REGION^93929', radius_from_location=0, )  # barnet
+    RightmovePropertiesForSale(location_identifier='REGION^1017', radius_from_location=1, )  # northwood
+    RightmovePropertiesForSale(location_identifier='REGION^1154', radius_from_location=1, )  # ruislip
+    RightmovePropertiesForSale(location_identifier='REGION^79781', radius_from_location=0.5, )  # harrow_on_the_hill
+
+    # Zoopla Properties
+    ZooplaPropertiesForSale(location_identifier='barnet-london-borough', radius_from_location=0, include_sstc=False)  # barnet
+    ZooplaPropertiesForSale(location_identifier='london/northwood', radius_from_location=1, include_sstc=False)  # northwood
+    ZooplaPropertiesForSale(location_identifier='ruislip', radius_from_location=1, include_sstc=False)  # ruislip
+    ZooplaPropertiesForSale(location_identifier='harrow-on-the-hill', radius_from_location=1, include_sstc=False)  # harrow-on-the-hill
+
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
