2bfbfd02bb92a66630904c72a5303ddea9febf36
Christopher Philp <chris.philp@starlingbank.com>
2021-11-19 18:25:27 +0000

    [main] | Encoding Pandas CSV to bytes

--- src/main.py
+++ src/main.py
@@ -51,7 +51,7 @@ def main():
     rightmove_csv = rightmove_csv.to_csv(index=False)
 
     # Update Rightmove CSV
-    rightmove_csv_encoded = base64.b64encode(rightmove_csv)
+    rightmove_csv_encoded = base64.b64encode(bytes(rightmove_csv, encoding='utf-8'))
     rightmove_commit_message = f"Updating rightmove-houses.csv - {dt.datetime.now().strftime('%d/%m/%Y')}"
     repo.update_file(path="rightmove-houses.csv", message=rightmove_commit_message, content=rightmove_csv_encoded, sha=repo_rightmove_csv.sha)
 
@@ -69,7 +69,7 @@ def main():
     zoopla_csv = zoopla_csv.to_csv(index=False)
 
     # Update Zoopla CSV
-    zoopla_csv_encoded = base64.b64encode(zoopla_csv)
+    zoopla_csv_encoded = base64.b64encode(bytes(zoopla_csv, encoding='utf-8'))
     zoopla_commit_message = f"Updating zoopla-houses.csv - {dt.datetime.now().strftime('%d/%m/%Y')}"
     repo.update_file(path="zoopla-houses.csv", message=zoopla_commit_message, content=zoopla_csv_encoded, sha=repo_zoopla_csv.sha)
 
