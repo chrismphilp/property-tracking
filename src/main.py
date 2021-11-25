760b75d90e2f7a03633b3d75df0f5fd24cad7e31
Christopher Philp <chris.philp@starlingbank.com>
2021-11-25 23:54:49 +0000

    [main] | Adding sorting before committing csv changes

--- src/main.py
+++ src/main.py
@@ -47,6 +47,7 @@ def main():
         RightmovePropertiesForSale(location_identifier='REGION^79781', radius_from_location=0.5, ).parse_site(),  # harrow_on_the_hill
     ])
     rightmove_csv = rightmove_csv.drop_duplicates(subset=rightmove_csv.columns.difference(["search_datetime", "added_on"]), keep="first")
+    rightmove_csv = rightmove_csv.sort_values("added_on", ascending=False)
     rightmove_csv = rightmove_csv.to_csv(index=False)
 
     # Update Rightmove CSV
@@ -64,6 +65,7 @@ def main():
         ZooplaPropertiesForSale(location_identifier='harrow-on-the-hill', radius_from_location=1, include_sstc=False).parse_site(),  # harrow-on-the-hill
     ])
     zoopla_csv = zoopla_csv.drop_duplicates(subset=zoopla_csv.columns.difference(["search_datetime", "added_on"]), keep="first")
+    zoopla_csv = zoopla_csv.sort_values("added_on", ascending=False)
     zoopla_csv = zoopla_csv.to_csv(index=False)
 
     # Update Zoopla CSV
